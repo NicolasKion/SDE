acf692d2ba7adbe9f06801490ee1d3778f0fc923
Tim Kunze <tim.kunze4@gmail.com>
2025-05-05 20:17:16 +0200

    Disable fk checks

--- src/Commands/Seed/SeedMarketGroupsCommand.php
+++ src/Commands/Seed/SeedMarketGroupsCommand.php
@@ -6,9 +6,11 @@ namespace NicolasKion\SDE\Commands\Seed;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 use NicolasKion\SDE\ClassResolver;
 use Symfony\Component\Yaml\Yaml;
+use Throwable;
 
 /**
  * @phpstan-type MarketGroupsFile array<int, array{
@@ -23,6 +25,9 @@ class SeedMarketGroupsCommand extends Command
 {
     protected $signature = 'sde:seed:market-groups';
 
+    /**
+     * @throws Throwable
+     */
     public function handle(): int
     {
         $file_name = 'sde/fsd/marketGroups.yaml';
@@ -30,11 +35,13 @@ class SeedMarketGroupsCommand extends Command
         /** @var MarketGroupsFile $data */
         $data = Yaml::parseFile(Storage::path($file_name));
 
-        $markeGroup = ClassResolver::marketGroup();
+        $marketGroup = ClassResolver::marketGroup();
 
-        DB::transaction(function () use ($data, $markeGroup) {
+        DB::transaction(function () use ($data, $marketGroup) {
+            Schema::disableForeignKeyConstraints();
+
             foreach ($data as $id => $values) {
-                $markeGroup::query()->updateOrInsert(['id' => $id], [
+                $marketGroup::query()->updateOrInsert(['id' => $id], [
                     'parent_id' => $values['parentGroupID'] ?? null,
                     'name' => $values['nameID']['en'],
                     'description' => $values['descriptionID']['en'] ?? null,
@@ -44,8 +51,10 @@ class SeedMarketGroupsCommand extends Command
                     'updated_at' => now(),
                 ]);
             }
+
+            Schema::enableForeignKeyConstraints();
+
         });
-        
         $this->info(sprintf('Successfully seeded %d market groups', count($data)));
 
         return self::SUCCESS;
